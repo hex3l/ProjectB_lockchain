27847cf64c15ff7a18474196d4b3325a81d94985
UnL0ckness <146536177+UnL0ckness@users.noreply.github.com>
2024-05-06 11:52:42 +0200

    feat: fix demo data

--- sql/dati-demo.sql
+++ sql/dati-demo.sql
@@ -16,14 +16,14 @@ INSERT INTO public.category
 INSERT INTO public.listing
 (id, id_creator, title, description, image, id_category, status, price, created, updated, "deletedAt") VALUES
 (1, 1, 'Meme Designer', 'Have you got an idea for a meme? I can make it for you.', '/assets/defaultLogos/x_memeDesigner.png', 1, 0, 0.1, '2024-01-28 23:22:56.953', '2024-01-28 23:22:56.953', NULL),
-(2, 1, 'The inspirated Bay', 'Team that can do everything.', '/assets/defaultLogos/x_theInspirateBay.png', 1, 0, 0.45, '2024-01-28 23:22:56.953', '2024-01-28 23:22:56.953', NULL),
-(3, 1, 'Pentester', 'Give me your website and I will try to hack it.', '/assets/defaultLogos/x_Pentester.png', 1, 0, 0.5, '2024-01-28 23:22:56.953', '2024-01-28 23:22:56.953', NULL),
-(4, 1, 'Pay To Do Nothing', 'Too much money? I can help you.', '/assets/defaultLogos/x_PayToDoNothing.png', 1, 0, 1, '2024-01-28 23:22:56.953', '2024-01-28 23:22:56.953', NULL),
-(5, 1, 'Logo Designer', 'Do you need a logo? try this service.', '/assets/defaultLogos/x_LogoDesigner.png', 1, 0, 0.1, '2024-01-28 23:22:56.953', '2024-01-28 23:22:56.953', NULL),
-(6, 1, 'Python developer', 'I can develop your Python application.', '/assets/defaultLogos/x_DataBaseManager.png', 1, 0, 0.1, '2024-01-28 23:22:56.953', '2024-01-28 23:22:56.953', NULL),
-(7, 1, '.Net Developer', 'I can develop your .Net application.', '/assets/defaultLogos/x_dotNetDeveloper.png', 1, 0, 0.2, '2024-01-28 23:22:56.953', '2024-01-28 23:22:56.953', NULL),
-(8, 1, 'Not Ready', 'Not Ready', '/assets/defaultLogos/x_NotReady.png', 1, 0, 0.1, '2024-01-28 23:22:56.953', '2024-01-28 23:22:56.953', NULL),
-(9, 1, 'DataBase Manager', 'Need to store data? This is the service for you.', '/assets/defaultLogos/x_DataBaseManager.png', 1, 0, 0.1, '2024-01-28 23:22:56.953', '2024-01-28 23:22:56.953', NULL),
-(10, 1, 'Solidity Contract Developer', 'I can develop your Solidity contract.', '/assets/defaultLogos/x_solidityContractDeveloper.png', 1, 0, 0.5, '2024-01-28 23:22:56.953', '2024-01-28 23:22:56.953', NULL),
-(11, 1, 'Oufit Maker', 'Send me a photo and I will make you an outfit.', '/assets/defaultLogos/X_OutfitMaker.png', 1, 0, 0.1, '2024-01-28 23:22:56.953', '2024-01-28 23:22:56.953', NULL),
-(12, 1, 'Plumber service', 'Plumber for you and your wife', '/assets/defaultLogos/x_plumber.png', 1, 0, 0.1, '2024-01-28 23:22:56.953', '2024-01-28 23:22:56.953', NULL);
+(2, 2, 'The inspirated Bay', 'Team that can do everything.', '/assets/defaultLogos/x_theInspirateBay.png', 2, 0, 0.45, '2024-01-28 23:22:56.953', '2024-01-28 23:22:56.953', NULL),
+(3, 3, 'Pentester', 'Give me your website and I will try to hack it.', '/assets/defaultLogos/x_Pentester.png', 3, 0, 0.5, '2024-01-28 23:22:56.953', '2024-01-28 23:22:56.953', NULL),
+(4, 2, 'Pay To Do Nothing', 'Too much money? I can help you.', '/assets/defaultLogos/x_PayToDoNothing.png', 4, 0, 1, '2024-01-28 23:22:56.953', '2024-01-28 23:22:56.953', NULL),
+(5, 1, 'Logo Designer', 'Do you need a logo? try this service.', '/assets/defaultLogos/x_LogoDesigner.png', 5, 0, 0.1, '2024-01-28 23:22:56.953', '2024-01-28 23:22:56.953', NULL),
+(6, 3, 'Python developer', 'I can develop your Python application.', '/assets/defaultLogos/x_DataBaseManager.png', 1, 0, 0.1, '2024-01-28 23:22:56.953', '2024-01-28 23:22:56.953', NULL),
+(7, 1, '.Net Developer', 'I can develop your .Net application.', '/assets/defaultLogos/x_dotNetDeveloper.png', 2, 0, 0.2, '2024-01-28 23:22:56.953', '2024-01-28 23:22:56.953', NULL),
+(8, 3, 'Not Ready', 'Not Ready', '/assets/defaultLogos/x_NotReady.png', 3, 0, 0.1, '2024-01-28 23:22:56.953', '2024-01-28 23:22:56.953', NULL),
+(9, 1, 'DataBase Manager', 'Need to store data? This is the service for you.', '/assets/defaultLogos/x_DataBaseManager.png', 4, 0, 0.1, '2024-01-28 23:22:56.953', '2024-01-28 23:22:56.953', NULL),
+(10, 2, 'Solidity Contract Developer', 'I can develop your Solidity contract.', '/assets/defaultLogos/x_solidityContractDeveloper.png', 5, 0, 0.5, '2024-01-28 23:22:56.953', '2024-01-28 23:22:56.953', NULL),
+(11, 2, 'Oufit Maker', 'Send me a photo and I will make you an outfit.', '/assets/defaultLogos/X_OutfitMaker.png', 3, 0, 0.1, '2024-01-28 23:22:56.953', '2024-01-28 23:22:56.953', NULL),
+(12, 1, 'Plumber service', 'Plumber for you and your wife', '/assets/defaultLogos/x_plumber.png', 3, 0, 0.1, '2024-01-28 23:22:56.953', '2024-01-28 23:22:56.953', NULL);
